9e77fae40d393f5c0cb4da678e11daa909d13a2e
pizi-0 <hafizirahim92@gmail.com>
2026-07-25 01:10:29 +0800

    feat: add continue watching

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,10 +1,12 @@
+import 'package:cached_network_image_ce/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:forui/forui.dart';
 import 'package:pudding/screens/home/home_provider.dart';
 import 'package:pudding/screens/home/models/home_data_model.dart';
-import 'package:pudding/screens/home/widgets/library_card.dart';
 import 'package:pudding/screens/home/widgets/showcase.dart';
+import 'package:pudding/utils/jellyfin_item_extensions.dart';
+import 'package:pudding/utils/jellyfin_view_extension.dart';
 
 class Home extends ConsumerStatefulWidget {
   const Home({super.key});
@@ -16,6 +18,7 @@ class Home extends ConsumerStatefulWidget {
 class _HomeState extends ConsumerState<Home> {
   @override
   Widget build(BuildContext context) {
+    final theme = FTheme.of(context);
     final data = ref.watch(homeProvider).value ?? HomeData();
     return TapRegion(
       onTapInside: (event) => FocusScope.of(context).unfocus(),
@@ -24,43 +27,77 @@ class _HomeState extends ConsumerState<Home> {
         child: CustomScrollView(
           slivers: [
             SliverFillViewport(
-              viewportFraction: 0.95,
+              viewportFraction: 1,
               padEnds: false,
               delegate: SliverChildListDelegate([
                 Showcase(),
               ]),
             ),
             SliverPadding(
-              padding: .all(20),
+              padding: .symmetric(vertical: 20, horizontal: 30),
               sliver: SliverMainAxisGroup(
                 slivers: [
                   PinnedHeaderSliver(
-                    child: FCard(child: Text('Libraries')),
+                    child: Text(
+                      'Libraries',
+                      style: theme.typography.display.xl3.copyWith(
+                        fontWeight: .bold,
+                        height: 1.5,
+                      ),
+                    ),
                   ),
-                  SliverPadding(padding: .all(5)),
+                  SliverToBoxAdapter(
+                    child: FDivider(
+                      style: .delta(
+                        padding: .value(.only(bottom: 8)),
+                      ),
+                    ),
+                  ),
                   SliverGrid.builder(
                     itemCount: data.libraries.length,
                     gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
                       maxCrossAxisExtent: 350,
-                      childAspectRatio: 16 / 9,
+                      childAspectRatio: 16 / 10,
                       mainAxisSpacing: 10,
                       crossAxisSpacing: 10,
                     ),
-                    itemBuilder: (context, index) =>
-                        LibraryCard(view: data.libraries[index]),
+                    itemBuilder: (context, index) => CachedNetworkImage(
+                      imageUrl: data.libraries[index].getPrimaryImage(),
+                      fit: .cover,
+                    ),
                   ),
                 ],
               ),
             ),
-            SliverMainAxisGroup(
-              slivers: [
-                PinnedHeaderSliver(
-                  child: Text('Continue watching'),
-                ),
-                SliverFillViewport(
-                  delegate: SliverChildListDelegate([Text('data')]),
-                ),
-              ],
+            SliverPadding(
+              padding: .symmetric(vertical: 20, horizontal: 30),
+              sliver: SliverMainAxisGroup(
+                slivers: [
+                  PinnedHeaderSliver(
+                    child: Text(
+                      'Continue watching',
+                      style: theme.typography.display.xl3.copyWith(
+                        fontWeight: .bold,
+                        height: 1.5,
+                      ),
+                    ),
+                  ),
+                  SliverPadding(padding: .all(5)),
+                  SliverGrid.builder(
+                    itemCount: data.continueWatching.length,
+                    gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 350,
+                      childAspectRatio: 16 / 10,
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 10,
+                    ),
+                    itemBuilder: (context, index) => CachedNetworkImage(
+                      imageUrl: data.continueWatching[index].getBackdrop(),
+                      fit: .cover,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
--- lib/screens/home/models/home_data_model.dart
+++ lib/screens/home/models/home_data_model.dart
@@ -5,16 +5,23 @@ import 'package:flutter/foundation.dart';
 class HomeData {
   final List<JellyfinItem> showcaseItem;
   final List<JellyfinView> libraries;
+  final List<JellyfinItem> continueWatching;
 
-  HomeData({this.showcaseItem = const [], this.libraries = const []});
+  HomeData({
+    this.showcaseItem = const [],
+    this.libraries = const [],
+    this.continueWatching = const [],
+  });
 
   HomeData copyWith({
     List<JellyfinItem>? showcaseItem,
     List<JellyfinView>? libraries,
+    List<JellyfinItem>? continueWatching,
   }) {
     return HomeData(
       showcaseItem: showcaseItem ?? this.showcaseItem,
       libraries: libraries ?? this.libraries,
+      continueWatching: continueWatching ?? this.continueWatching,
     );
   }
 
@@ -23,9 +30,11 @@ class HomeData {
     if (identical(this, other)) return true;
 
     return listEquals(other.showcaseItem, showcaseItem) &&
-        listEquals(other.libraries, libraries);
+        listEquals(other.libraries, libraries) &&
+        listEquals(other.continueWatching, continueWatching);
   }
 
   @override
-  int get hashCode => showcaseItem.hashCode ^ libraries.hashCode;
+  int get hashCode =>
+      showcaseItem.hashCode ^ libraries.hashCode ^ continueWatching.hashCode;
 }
