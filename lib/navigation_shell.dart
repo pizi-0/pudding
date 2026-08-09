319f2289026e982cbbbe95123a849ef165826ca3
pizi-0 <hafizirahim92@gmail.com>
2026-08-09 15:28:26 +0800

    feat: add mouse back button support

--- lib/navigation_shell.dart
+++ lib/navigation_shell.dart
@@ -1,4 +1,5 @@
 import 'package:collection/collection.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:forui/forui.dart';
@@ -20,100 +21,113 @@ class MainNavigationShell extends ConsumerWidget {
     final theme = FTheme.of(context);
     final bottomBar = size.width <= theme.breakpoints.md;
 
-    return FScaffold(
-      childPad: false,
-      child: Stack(
-        children: [
-          Column(
-            children: [
-              Expanded(
-                child: AnimatedBranchContainer(
-                  currentIndex: navigationShell.currentIndex,
-                  children: children,
-                ),
-              ),
-              AnimatedSwitcher(
-                duration: kDefaultAnimationDuration,
-                child: bottomBar
-                    ? FBottomNavigationBar(
-                        index: navigationShell.currentIndex,
-                        onChange: (value) {
-                          navigationShell.goBranch(
-                            value,
-                            initialLocation:
-                                value == navigationShell.currentIndex,
-                          );
-                        },
-                        children: [
-                          FBottomNavigationBarItem(
-                            icon: Icon(FLucideIcons.home),
-                          ),
-                          FBottomNavigationBarItem(
-                            icon: Icon(FLucideIcons.library),
-                          ),
-                          FBottomNavigationBarItem(
-                            icon: Icon(FLucideIcons.settings),
-                          ),
-                          FBottomNavigationBarItem(
-                            icon: Icon(FLucideIcons.user),
-                          ),
-                        ],
-                      )
-                    : SizedBox.shrink(),
-              ),
-            ],
-          ),
-          Positioned(
-            left: 0,
-            top: 0,
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: IntrinsicWidth(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: theme.colors.background.withAlpha(220),
-                    borderRadius: theme.style.borderRadius.lg,
-                    backgroundBlendMode: .darken,
+    return Listener(
+      onPointerDown: (event) {
+        if (event.buttons == kBackMouseButton) {
+          if (context.canPop()) {
+            context.pop();
+          } else {
+            context.go('/');
+          }
+        }
+      },
+      child: FScaffold(
+        childPad: false,
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Expanded(
+                  child: AnimatedBranchContainer(
+                    currentIndex: navigationShell.currentIndex,
+                    children: children,
                   ),
-                  padding: .all(4),
-                  child: Row(
-                    mainAxisSize: .min,
-                    spacing: 10,
-                    crossAxisAlignment: .center,
-                    children: [
-                      FButton(
-                        selected: navigationShell.currentIndex == 0,
-                        variant: navigationShell.currentIndex == 0
-                            ? .primary
-                            : .ghost,
-                        onPress: () {
-                          navigationShell.goBranch(
-                            0,
-                            initialLocation: navigationShell.currentIndex == 0,
-                          );
-                        },
-                        prefix: Icon(FLucideIcons.home),
-                        child: Text('Home'),
-                      ),
-                      FButton(
-                        selected: navigationShell.currentIndex == 1,
-                        variant: .ghost,
-                        onPress: () {
-                          navigationShell.goBranch(
-                            1,
-                            initialLocation: navigationShell.currentIndex == 1,
-                          );
-                        },
-                        prefix: Icon(FLucideIcons.library),
-                        child: Text('Libraries'),
-                      ),
-                    ],
+                ),
+                AnimatedSwitcher(
+                  duration: kDefaultAnimationDuration,
+                  child: bottomBar
+                      ? FBottomNavigationBar(
+                          index: navigationShell.currentIndex,
+                          onChange: (value) {
+                            navigationShell.goBranch(
+                              value,
+                              initialLocation:
+                                  value == navigationShell.currentIndex,
+                            );
+                          },
+                          children: [
+                            FBottomNavigationBarItem(
+                              icon: Icon(FLucideIcons.home),
+                            ),
+                            FBottomNavigationBarItem(
+                              icon: Icon(FLucideIcons.library),
+                            ),
+                            FBottomNavigationBarItem(
+                              icon: Icon(FLucideIcons.settings),
+                            ),
+                            FBottomNavigationBarItem(
+                              icon: Icon(FLucideIcons.user),
+                            ),
+                          ],
+                        )
+                      : SizedBox.shrink(),
+                ),
+              ],
+            ),
+            Positioned(
+              left: 0,
+              top: 0,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: IntrinsicWidth(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: theme.colors.background.withAlpha(220),
+                      borderRadius: theme.style.borderRadius.lg,
+                      backgroundBlendMode: .darken,
+                    ),
+                    padding: .all(4),
+                    child: Row(
+                      mainAxisSize: .min,
+                      spacing: 10,
+                      crossAxisAlignment: .center,
+                      children: [
+                        FButton(
+                          selected: navigationShell.currentIndex == 0,
+                          variant: navigationShell.currentIndex == 0
+                              ? .primary
+                              : .ghost,
+                          onPress: () {
+                            navigationShell.goBranch(
+                              0,
+                              initialLocation:
+                                  navigationShell.currentIndex == 0,
+                            );
+                          },
+                          prefix: Icon(FLucideIcons.home),
+                          child: Text('Home'),
+                        ),
+                        FButton(
+                          selected: navigationShell.currentIndex == 1,
+                          variant: .ghost,
+                          onPress: () {
+                            navigationShell.goBranch(
+                              1,
+                              initialLocation:
+                                  navigationShell.currentIndex == 1,
+                            );
+                          },
+                          prefix: Icon(FLucideIcons.library),
+                          child: Text('Libraries'),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
