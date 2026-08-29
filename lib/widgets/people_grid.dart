430c2f8ae4367100b4ffc81e04a33a699d857b10
pizi-0 <hafizirahim92@gmail.com>
2026-08-29 21:43:24 +0800

    misc: add repaintboundary

--- lib/widgets/people_grid.dart
+++ lib/widgets/people_grid.dart
@@ -160,86 +160,88 @@ class _PeopleButtonState extends State<PeopleButton> {
     final theme = FTheme.of(context);
     final style = theme.style;
 
-    return FButton.raw(
-      variant: .outline,
-      onPress: () {},
-      onHoverChange: (value) => setState(() => hover = !hover),
-      onFocusChange: (value) => setState(() => hover = !hover),
-      child: Padding(
-        padding: const EdgeInsets.all(2.0),
-        child: ClipRRect(
-          borderRadius: style.borderRadius.sm,
-          child: Stack(
-            fit: .expand,
-            children: [
-              Positioned.fill(
-                child: AnimatedScale(
-                  duration: kDefaultAnimationDuration,
-                  scale: hover ? 1.02 : 1,
-                  alignment: .bottomCenter,
-                  child: CachedNetworkImage(
-                    imageUrl: services<JellyfinClient>().images.url(
-                      itemId: widget.people.Id,
-                    ),
-                    fit: .cover,
-                    memCacheHeight: 400,
-                    errorBuilder: (context, error, stackTrace) => Container(
-                      color: Colors.black,
-                      child: Icon(
-                        FPhosphorBoldIcons.user,
-                        size: 50,
+    return RepaintBoundary(
+      child: FButton.raw(
+        variant: .outline,
+        onPress: () {},
+        onHoverChange: (value) => setState(() => hover = !hover),
+        onFocusChange: (value) => setState(() => hover = !hover),
+        child: Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: ClipRRect(
+            borderRadius: style.borderRadius.sm,
+            child: Stack(
+              fit: .expand,
+              children: [
+                Positioned.fill(
+                  child: AnimatedScale(
+                    duration: kDefaultAnimationDuration,
+                    scale: hover ? 1.02 : 1,
+                    alignment: .bottomCenter,
+                    child: CachedNetworkImage(
+                      imageUrl: services<JellyfinClient>().images.url(
+                        itemId: widget.people.Id,
+                      ),
+                      fit: .cover,
+                      memCacheHeight: 400,
+                      errorBuilder: (context, error, stackTrace) => Container(
+                        color: Colors.black,
+                        child: Icon(
+                          FPhosphorBoldIcons.user,
+                          size: 50,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              Positioned.fill(
-                top: -1,
-                left: -1,
-                right: -1,
-                bottom: -1,
-                child: DecoratedBox(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [Colors.transparent, theme.colors.background],
-                      begin: .center,
-                      end: .bottomCenter,
+                Positioned.fill(
+                  top: -1,
+                  left: -1,
+                  right: -1,
+                  bottom: -1,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [Colors.transparent, theme.colors.background],
+                        begin: .center,
+                        end: .bottomCenter,
+                      ),
                     ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      mainAxisAlignment: .end,
-                      children: [
-                        AnimatedSize(
-                          duration: kDefaultAnimationDuration,
-                          alignment: .topCenter,
-                          child: Column(
-                            mainAxisSize: .min,
-                            children: [
-                              Text(
-                                widget.people.Name,
-                                maxLines: hover ? null : 1,
-                                overflow: hover ? null : .ellipsis,
-                                textAlign: .center,
-                                style: theme.typography.body.sm,
-                              ).bold(),
-                              Text(
-                                'as ${widget.people.Role}',
-                                maxLines: hover ? null : 1,
-                                overflow: hover ? null : .ellipsis,
-                                textAlign: .center,
-                                style: theme.typography.body.xs,
-                              ),
-                            ],
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: .end,
+                        children: [
+                          AnimatedSize(
+                            duration: kDefaultAnimationDuration,
+                            alignment: .topCenter,
+                            child: Column(
+                              mainAxisSize: .min,
+                              children: [
+                                Text(
+                                  widget.people.Name,
+                                  maxLines: hover ? null : 1,
+                                  overflow: hover ? null : .ellipsis,
+                                  textAlign: .center,
+                                  style: theme.typography.body.sm,
+                                ).bold(),
+                                Text(
+                                  'as ${widget.people.Role}',
+                                  maxLines: hover ? null : 1,
+                                  overflow: hover ? null : .ellipsis,
+                                  textAlign: .center,
+                                  style: theme.typography.body.xs,
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
