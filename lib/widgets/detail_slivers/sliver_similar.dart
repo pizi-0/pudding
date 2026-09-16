b5a8297edd5a58925ed373ca7dfc422a5d27a841
pizi-0 <hafizirahim92@gmail.com>
2026-09-16 18:22:36 +0800

    feat: allow additional nav stack when navigating from 'More like this'

--- lib/widgets/detail_slivers/sliver_similar.dart
+++ lib/widgets/detail_slivers/sliver_similar.dart
@@ -1,6 +1,7 @@
 import 'package:dart_jellyfin/dart_jellyfin.dart';
 import 'package:flutter/material.dart';
 import 'package:forui/forui.dart';
+import 'package:go_router/go_router.dart';
 import 'package:pudding/utils/scroll_to_key_extension.dart';
 import 'package:pudding/widgets/detail_slivers/sliver_item_grid.dart';
 import 'package:pudding/widgets/detail_slivers/sliver_section.dart';
@@ -11,6 +12,11 @@ class SliverSimilar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final matches = GoRouter.of(context)
+        .routerDelegate
+        .currentConfiguration
+        .matches;
+
     return SliverSection(
       header: FButton(
         variant: .outline,
@@ -22,7 +28,10 @@ class SliverSimilar extends StatelessWidget {
         child: Text('More like this'),
       ),
       slivers: [
-        SliverItemGrid(items: items),
+        SliverItemGrid(
+          items: items,
+          shouldReplace: matches.length > 3,
+        ),
       ],
     );
   }
