0e264f301c9e410b443458ba3751b251843bb16e
pizi-0 <hafizirahim92@gmail.com>
2026-09-19 12:36:28 +0800

    fix: disallow for played and unplayed filter to be selected at the same time.

--- lib/screens/library_detail/widget/sliver_library_item_grid.dart
+++ lib/screens/library_detail/widget/sliver_library_item_grid.dart
@@ -108,261 +108,6 @@ class _SliverLibraryItemGridState extends ConsumerState<SliverLibraryItemGrid> {
         ),
       ],
     );
-
-    // return SliverMainAxisGroup(
-    //   slivers: [
-    //     PinnedHeaderSliver(
-    //       child: Bar(
-    //         padding: .fromLTRB(20, 0, 20, 0),
-    //         child: SectionHeader(
-    //           title: Row(
-    //             spacing: 4,
-    //             children: [
-    //               Text('All'),
-    //               FButton.icon(
-    //                 variant: .ghost,
-    //                 onPress: () {
-    //                   showFSheet(
-    //                     context: context,
-    //                     builder: (context) => FilterSheet(id: widget.id),
-    //                     side: .ltr,
-    //                   );
-    //                 },
-    //                 onSecondaryPress: () {
-    //                   libNotifier
-    //                     ..resetFilter()
-    //                     ..refresh();
-    //                 },
-    //                 child: Icon(
-    //                   data.filters.isEmpty
-    //                       ? FPhosphorBoldIcons.funnel
-    //                       : FPhosphorBoldIcons.funnelX,
-    //                 ),
-    //               ),
-    //             ],
-    //           ),
-    //           subtitle: Row(
-    //             spacing: 10,
-    //             children: [
-    //               Text(
-    //                 'Showing ${data.items.length} of ${data.count}',
-    //                 style: theme.typography.body.xs.copyWith(
-    //                   color: theme.colors.mutedForeground,
-    //                 ),
-    //               ),
-    //               if (!data.filters.isEmpty) Text('[Filtered]'),
-    //               if (libAsync.isLoading)
-    //                 FCircularProgress(
-    //                   style: .delta(
-    //                     iconStyle: .delta(
-    //                       color: theme.colors.primary,
-    //                       size: theme.typography.body.xs.fontSize,
-    //                     ),
-    //                   ),
-    //                 ),
-    //             ],
-    //           ),
-    //           trailings: [
-    //             Container(
-    //               decoration: BoxDecoration(
-    //                 border: .all(
-    //                   color: theme.colors.border,
-    //                   width: 2,
-    //                 ),
-    //                 borderRadius: theme.style.borderRadius.md,
-    //               ),
-    //               height: 36,
-    //               child: Row(
-    //                 spacing: 10,
-    //                 children: [
-    //                   FTooltip(
-    //                     hover: false,
-    //                     tipBuilder: (context, controller) =>
-    //                         Text(prefs.maxImageWidth.toStringAsFixed(0)),
-    //                     builder: (context, controller, child) {
-    //                       return FButton.icon(
-    //                         size: .sm,
-    //                         variant: .ghost,
-    //                         style: .delta(
-    //                           decoration: .delta([
-    //                             .all(
-    //                               .boxDelta(
-    //                                 borderRadius: theme.style.borderRadius.sm,
-    //                               ),
-    //                             ),
-    //                           ]),
-    //                         ),
-    //                         onHoverChange: (h) {
-    //                           if (h) {
-    //                             controller.show();
-    //                           } else {
-    //                             controller.hide();
-    //                           }
-    //                         },
-    //                         onPress: () {
-    //                           if (libAsync.isLoading) return;
-    //                           controller.show();
-
-    //                           libNotifier
-    //                             ..setMaxImageWidth(
-    //                               (prefs.maxImageWidth - 10).clamp(150, 650),
-    //                             )
-    //                             ..updateDisplayPrefs();
-    //                         },
-    //                         child: Icon(
-    //                           FPhosphorBoldIcons.magnifyingGlassMinus,
-    //                         ),
-    //                       );
-    //                     },
-    //                   ),
-    //                   SizedBox(
-    //                     width: 120,
-    //                     child: FSlider(
-    //                       enabled: !libAsync.isLoading,
-    //                       control: .liftedDiscrete(
-    //                         interaction: .tapAndSlideThumb,
-    //                         value: FSliderValue(
-    //                           max: ((prefs.maxImageWidth - 150) / 500).clamp(
-    //                             0,
-    //                             1,
-    //                           ),
-    //                         ),
-    //                         onChange: (value) {
-    //                           if (libAsync.isLoading) {
-    //                             return;
-    //                           }
-    //                           libNotifier.setMaxImageWidth(
-    //                             (value.max * 500) + 150,
-    //                           );
-    //                         },
-    //                       ),
-    //                       onEnd: (value) {
-    //                         libNotifier.updateDisplayPrefs();
-    //                       },
-    //                       marks: marks
-    //                           .map(
-    //                             (e) => FSliderMark(value: e),
-    //                           )
-    //                           .toList(),
-    //                       style: .delta(
-    //                         childPadding: .value(.zero),
-    //                       ),
-    //                       tooltipBuilder: (controller, value) => Text(
-    //                         ((value * 500) + 150).toStringAsFixed(0),
-    //                       ),
-    //                     ),
-    //                   ),
-    //                   FTooltip(
-    //                     hover: false,
-    //                     tipBuilder: (context, controller) =>
-    //                         Text(prefs.maxImageWidth.toStringAsFixed(0)),
-    //                     builder: (context, controller, child) {
-    //                       return FButton.icon(
-    //                         size: .sm,
-    //                         style: .delta(
-    //                           decoration: .delta([
-    //                             .all(
-    //                               .boxDelta(
-    //                                 borderRadius: theme.style.borderRadius.sm,
-    //                               ),
-    //                             ),
-    //                           ]),
-    //                         ),
-    //                         variant: .ghost,
-    //                         onHoverChange: (h) {
-    //                           if (h) {
-    //                             controller.show();
-    //                           } else {
-    //                             controller.hide();
-    //                           }
-    //                         },
-    //                         onPress: () {
-    //                           if (libAsync.isLoading) return;
-    //                           controller.show();
-
-    //                           final double maxWidth = (prefs.maxImageWidth + 10)
-    //                               .clamp(150, 650);
-
-    //                           libNotifier
-    //                             ..setMaxImageWidth(maxWidth)
-    //                             ..updateDisplayPrefs();
-    //                         },
-    //                         child: Icon(FPhosphorBoldIcons.magnifyingGlassPlus),
-    //                       );
-    //                     },
-    //                   ),
-    //                 ],
-    //               ),
-    //             ),
-    //             FPopover(
-    //               style: .delta(
-    //                 barrierFilter: (context, animation) => ImageFilter.compose(
-    //                   outer: ImageFilter.blur(),
-    //                   inner: ColorFilter.mode(
-    //                     Color.lerp(
-    //                       const Color(0x00000000),
-    //                       Colors.black.withAlpha(200),
-    //                       animation,
-    //                     )!,
-    //                     BlendMode.srcOver,
-    //                   ),
-    //                 ),
-    //               ),
-    //               builder: (context, controller, child) => FButton.icon(
-    //                 onPress: controller.toggle,
-    //                 child: AnimatedMorphIcon(
-    //                   icon: prefs.isPoster
-    //                       ? FLucideIcons.rectangleVertical
-    //                       : prefs.isThumb
-    //                       ? FLucideIcons.rectangleHorizontal
-    //                       : FLucideIcons.square,
-    //                 ),
-    //               ),
-    //               popoverBuilder: (context, controller) {
-    //                 final viewList = [vtPoster, vtThumb, vtSquare];
-
-    //                 return FCard(
-    //                   child: Padding(
-    //                     padding: const EdgeInsets.all(4.0),
-    //                     child: Column(
-    //                       spacing: 4,
-    //                       children: viewList
-    //                           .map(
-    //                             (e) => FButton(
-    //                               variant: e == prefs.puddingLibraryViewType
-    //                                   ? .primary
-    //                                   : .outline,
-    //                               prefix: Icon(
-    //                                 e == vtPoster
-    //                                     ? FLucideIcons.rectangleVertical
-    //                                     : e == vtThumb
-    //                                     ? FLucideIcons.rectangleHorizontal
-    //                                     : FLucideIcons.square,
-    //                               ),
-    //                               onPress: () {
-    //                                 controller.hide();
-    //                                 if (libAsync.isLoading) return;
-
-    //                                 libNotifier
-    //                                   ..setViewType(e)
-    //                                   ..updateDisplayPrefs();
-    //                               },
-    //                               child: Text(e),
-    //                             ),
-    //                           )
-    //                           .toList(),
-    //                     ),
-    //                   ),
-    //                 );
-    //               },
-    //             ),
-    //           ],
-    //         ),
-    //       ),
-    //     ),
-
-    //   ],
-    // );
   }
 }
 
@@ -452,6 +197,14 @@ class _FilterSheetState extends ConsumerState<FilterSheet> {
                           .toList(),
                       // enabled: !libAsync.isLoading,
                       onChanged: (result) {
+                        if (result.lastOrNull?.toString() ==
+                            JellyFilter.played.value) {
+                          result.remove(JellyFilter.unplayed.value);
+                        } else if (result.lastOrNull?.toString() ==
+                            JellyFilter.unplayed.value) {
+                          result.remove(JellyFilter.played.value);
+                        }
+
                         libNotifier
                           ..applyFilter(filters: result)
                           ..refresh();
@@ -549,7 +302,7 @@ class SliverRadioState<T> extends State<SliverRadio<T>> {
   @override
   void didUpdateWidget(covariant SliverRadio<T> oldWidget) {
     if (!listEquals(oldWidget.initialValues, widget.initialValues)) {
-      filterList = widget.initialValues;
+      filterList = List<T>.from(widget.initialValues);
       setState(() {});
     }
     super.didUpdateWidget(oldWidget);
